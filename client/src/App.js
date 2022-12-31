73e14d13fd2f7a64a5b2e0d1d8fd7f9ee1e28507
Armaan Kara <vancouverarmaan@gmail.com>
2022-12-30 16:06:59 -0800

    updated front end code to fix deploy issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,111 +1,122 @@
-import  Axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import Footer from './components/Footer'
-import Note from './components/Note'
-import Notification from './components/Notification'
-import noteService from './services/notes'
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Footer from './components/Footer';
+import Note from './components/Note';
+import Notification from './components/Notification';
+import noteService from './services/notes';
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
   // const [showAll, setShowAll] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
+  // const [errorMessage, setErrorMessage] = useState(null);
   const [popupActive, setPopupActive] = useState(false);
+  const useNoteService = noteService();
 
   useEffect(() => {
-    noteService
-      .getAll()
-      .then(initialNotes => {
-      setNotes(initialNotes)
-    })
-  }, [])
-
+    useNoteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
-    }
+      date: new Date().toISOString()
+    };
 
-    noteService
+    useNoteService
       .create(noteObject)
-        .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
-        setNewNote('')
-      })
-  }
-
-
+      .then((returnedNote) => {
+        setNotes(notes.concat(returnedNote));
+        setNewNote('');
+      });
+  };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
   const deleteNotes = (id) => {
     Axios.delete(`/api/notes/${id}`).then(() => {
-      setNotes(notes => notes.filter(note => note.id !== id))
-    })
-  }
-  
-  const completeNote = (id) =>{
-    const data =  Axios.put(`/api/notes/${id}`).then(
-      setNotes(notes => notes.map(note => {
-        if (note.id === data.id){
-          note.complete = data.complete
-        }
-        return note;
-      }))
-    )
-  }
+      setNotes((notes) =>
+        notes.filter((note) => note.id !== id)
+      );
+    });
+  };
+
+  const completeNote = (id) => {
+    const data = Axios.put(`/api/notes/${id}`).then(
+      setNotes((notes) =>
+        notes.map((note) => {
+          if (note.id === data.id) {
+            note.complete = data.complete;
+          }
+          return note;
+        })
+      )
+    );
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Grocery List</h1>
-      <Notification message={errorMessage} /> 
+      <Notification message={null} />
       <ul>
-        {notes.length > 0 ? notes.map(note => (
+        {notes.length > 0 ? (
+          notes.map((note) => (
             <Note
               key={note.id}
-              note={note} 
+              note={note}
               deleteNotes={deleteNotes}
               completeNote={completeNote}
             />
-        )) : (
-          <p>You currently have no tasks, click the plus button to add items.</p>
+          ))
+        ) : (
+          <p>
+            You currently have no tasks, click the plus
+            button to add items.
+          </p>
         )}
       </ul>
       <div>
-            <div className="addPopup" onClick={() => setPopupActive(true)}><i class="fas fa-plus"></i></div>
-    
-            {popupActive ? (
-                <div className="popup">
-                    <div className="closePopup" onClick={() => setPopupActive(false)}><i class="fas fa-times"></i></div>
-                    <div className="content">
-                        <h3>Add Item</h3>
-                        <form onSubmit={addNote}>
-                            <input 
-                                type="text" 
-                                className="add-list-input" 
-                                value={newNote}
-                                onChange={handleNoteChange}
-                            />
-                            <br/>
-                            <button type="submit">add</button>
-                        </form>
-                        
-                    </div>
-                </div>
-            ) : ''}
+        <div
+          className='addPopup'
+          onClick={() => setPopupActive(true)}>
+          <i class='fas fa-plus'></i>
         </div>
 
-      <Footer />  
+        {popupActive ? (
+          <div className='popup'>
+            <div
+              className='closePopup'
+              onClick={() => setPopupActive(false)}>
+              <i class='fas fa-times'></i>
+            </div>
+            <div className='content'>
+              <h3>Add Item</h3>
+              <form onSubmit={addNote}>
+                <input
+                  type='text'
+                  className='add-list-input'
+                  value={newNote}
+                  onChange={handleNoteChange}
+                />
+                <br />
+                <button type='submit'>add</button>
+              </form>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-
-
+export default App;
--- client/src/services/notes.js
+++ client/src/services/notes.js
@@ -1,24 +1,29 @@
-import axios from 'axios'
-const baseUrl = '/api/notes'
+import axios from 'axios';
+const baseUrl = '/api/notes';
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const noteService = () => {
+  const getAll = () => {
+    const request = axios.get(baseUrl);
+    return request.then((response) => response.data);
+  };
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+  const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject);
+    return request.then((response) => response.data);
+  };
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
+  const update = (id, newObject) => {
+    const request = axios.put(
+      `${baseUrl}/${id}`,
+      newObject
+    );
+    return request.then((response) => response.data);
+  };
 
-// const deleteNote = async id => {
-//   const data = await fetch(`${baseUrl}/${id}`, { method: "DELETE" }).then(response => response.json());
-// }
-  
+  // const deleteNote = async id => {
+  //   const data = await fetch(`${baseUrl}/${id}`, { method: "DELETE" }).then(response => response.json());
+  // }
 
-export default { getAll, create, update }
+  return { getAll, create, update };
+};
+export default noteService;
